cd675ea7b64642947429dad5e5791d66eb3965ab
uink45 <79078981+uink45@users.noreply.github.com>
2023-06-12 11:37:06 +1000

    Update password_replacement.sh

--- consensus/genesis_validators/password_replacement.sh
+++ consensus/genesis_validators/password_replacement.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Configuration
-search_dir="/Users/multi/Documents/Projects/Devnet/piertwo-devnet2/consensus/genesis_validators/validator_keys"
-keystore_pass_file="/Users/multi/Documents/Projects/Devnet/piertwo-devnet2/consensus/genesis_validators/passwords/keystore_pass.txt"
+search_dir="./validator_keys"
+keystore_pass_file="./passwords/keystore_pass.txt"
 save_dir="$(dirname "${keystore_pass_file}")"
 
 # Create new files with identical content to keystore_pass.txt
